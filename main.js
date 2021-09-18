930cba68abb5acbeaf968deb0bad34545f53a396
shawn2james <shawwn2james@gmail.com>
2021-09-18 19:31:44 +0530

    tried javascript for raccessing grids

---
+++ main.js
@@ -0,0 +1,9 @@
+const cells = []
+
+const notes = document.querySelector(".notes");
+const widths = Array.from(notes.children).map(note => {
+    const cell = {
+        
+    }
+});
+console.log(widths);
